be40b9c0f6d479d6875e892aae31a6f7247bf589
Omkar Kulkarni <omkarkulkarni923@gmail.com>
2026-05-09 10:21:27 +0530

    fix: add eslint config for backend

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2020: true },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended'],
+  rules: {},
+}
